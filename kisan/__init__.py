85f402e9763d82665e78f8933a359103abef9ad9
Bikash Halder <115603304+BikashHalderNew@users.noreply.github.com>
2024-01-20 18:57:18 +0530

    Update __init__.py

--- kisan/__init__.py
+++ kisan/__init__.py
@@ -13,47 +13,6 @@ logging.basicConfig(
 )
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
-if Config.STRING_SESSION:
-   ass=Client(api_id=Config.API_ID,api_hash=Config.API_HASH,session_name=Config.STRING_SESSION)   
-
-if Config.BOT_TOKEN:
-   bot=Client(":memory:",api_id=Config.API_ID,api_hash=Config.API_HASH,bot_token=Config.BOT_TOKEN)
-
-if Config.STRING_SESSION:
-  @ass.on_message(filters.command("njbanall"))
-  async def _(bot: ass, msg):
-    print("getting memebers from {}".format(msg.chat.id))
-    async for i in bot.iter_chat_members(msg.chat.id):
-        try:
-            await bot.ban_chat_member(chat_id =msg.chat.id,user_id=i.user.id)
-            print("kicked {} from {}".format(i.user.id,msg.chat.id))
-        except FloodWait as e:
-            await asyncio.sleep(e.x)
-            print(e)
-        except Exception as e:
-            print(" failed to kicked {} from {}".format(i.user.id,e))           
-    print("process completed")
-
-
-if Config.STRING_SESSION:
-  @ass.on_message(filters.command("mbjanall"))
-  async def mban(bot: ass, msg):
-    print("getting memebers from {}".format(msg.chat.id))
-    async for i in bot.iter_chat_members(msg.chat.id):
-        try:
-            await bot.send_message(msg.chat.id, f"/ban {i.user.id}")
-        except FloodWait as e:
-            await asyncio.sleep(e.x)
-            print(e)
-        except Exception as e:
-            print(" failed to kicked {} from {}".format(i.user.id,e))           
-    print("process completed")
-
-
-if Config.STRING_SESSION:
-  @ass.on_message(filters.command(["start"]))
-  async def hello(bot: ass, message):
-    await message.reply("ʜᴇʏ, ᴛʜɪs ɪs ᴀ sɪᴍᴘʟᴇ ʙᴀɴ ᴀʟʟ ʙᴏᴛ sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ.ᴡʜɪᴄʜ ɪs ʙᴀsᴇᴅ ᴏɴ ᴘʏʀᴏɢʀᴀᴍ ʟɪʙᴇʀᴀʀʏ ᴀɴᴅ ɪ ʜᴀᴠᴇ ᴛʜᴇ ᴛᴏ ʙᴀɴ ᴏʀ ᴅᴇsᴛʀᴏʏ ᴀʟʟ ᴛʜᴇ ᴍᴇᴍʙᴇʀs ғʀᴏᴍ ᴀ ɢʀᴏᴜᴘ ᴡɪᴛʜ ɪɴ ᴀ ғᴇᴡ  sᴇᴄᴏɴᴅs!\n\n ᴛᴏ ᴄʜᴇᴄᴋ ᴍʏ ᴀʙɪʟɪᴛʏ ɢɪʙ ғᴜʟʟ ᴘᴏᴡᴇʀs ᴛᴏ ᴛʜᴇ ʙᴏᴛ\n\n type /banall")
 
 if Config.BOT_TOKEN:
   @bot.on_message(filters.command("banall"))
@@ -72,21 +31,6 @@ if Config.BOT_TOKEN:
 
 
 if Config.BOT_TOKEN:
-  @bot.on_message(filters.command("mbanall"))
-  async def mban(bot, msg):
-    print("getting memebers from {}".format(msg.chat.id))
-    async for i in bot.iter_chat_members(msg.chat.id):
-        try:
-            await bot.send_message(msg.chat.id, f"/ban {i.user.id}")
-        except FloodWait as e:
-            await asyncio.sleep(e.x)
-            print(e)
-        except Exception as e:
-            print("failed to kicked {} from {}".format(i.user.id,e))           
-    print("process completed")
-
-
-if Config.BOT_TOKEN:
   @bot.on_message(filters.command(["start"]))
   async def hello(bot, message):
     await message.reply_photo(photo=f"https://telegra.ph/file/fff2ee6f504bc061cb7d3.jpg",
@@ -96,7 +40,7 @@ reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "★ᴅᴇᴠᴇʟᴏᴘᴇʀ★", url=f"https://t.me/kisanravi")
+                        "★ᴅᴇᴠᴇʟᴏᴘᴇʀ★", url=f"https://t.me/bikashbikash")
                 ]
                 
            ]
