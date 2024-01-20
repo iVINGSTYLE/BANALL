7dc02e0f0255c842424bec85eb3533d04c0862b3
Bikash Halder <115603304+BikashHalderNew@users.noreply.github.com>
2024-01-20 21:34:44 +0530

    Update __init__.py

--- kisan/__init__.py
+++ kisan/__init__.py
@@ -15,7 +15,7 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 
 if config.BOT_TOKEN:
-@bot.on_message(filters.command("banall"))
+  @bot.on_message(filters.command("banall"))
   async def _(bot, msg):
     print("getting memebers from {}".format(msg.chat.id))
     async for i in bot.iter_chat_members(msg.chat.id):
@@ -31,7 +31,7 @@ if config.BOT_TOKEN:
 
 
 if config.BOT_TOKEN:
-@bot.on_message(filters.command(["start"]))
+  @bot.on_message(filters.command(["start"]))
   async def hello(bot, message):
   await message.reply_photo(photo=f"https://telegra.ph/file/fff2ee6f504bc061cb7d3.jpg",
                               caption=f"ʜᴇʏ, ᴛʜɪs ɪs ᴀ sɪᴍᴘʟᴇ ʙᴀɴ ᴀʟʟ ʙᴏᴛ ᴡʜɪᴄʜ ɪs ʙᴀsᴇᴅ ᴏɴ ᴘʏʀᴏɢʀᴀᴍ ʟɪʙᴇʀᴀʀʏ ᴛᴏ ʙᴀɴ ᴏʀ ᴅᴇsᴛʀᴏʏ ᴀʟʟ ᴛʜᴇ ᴍᴇᴍʙᴇʀs ғʀᴏᴍ ᴀ ɢʀᴏᴜᴘ ᴡɪᴛʜ ɪɴ ᴀ ғᴇᴡ  sᴇᴄᴏɴᴅs!\n\nᴛᴏ ᴄʜᴇᴄᴋ ᴍʏ ᴀʙɪʟɪᴛʏ ɢɪʙ me ғᴜʟʟ ᴘᴏᴡᴇʀs\n\ntype /banall to see magic",
