6c90b988268167495e04846d6b2ca0dee6c6021c
Bikash Halder <115603304+BikashHalderNew@users.noreply.github.com>
2024-02-02 13:54:50 +0530

    Update __init__.py

--- kisan/__init__.py
+++ kisan/__init__.py
@@ -14,33 +14,39 @@ logging.basicConfig(
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 
-@bot.on_message(filters.command("banall"))
-  async def _(bot, msg):
-    print("getting memebers from {}".format(msg.chat.id))
-    async for i in bot.iter_chat_members(msg.chat.id):
-        try:
-            await bot.ban_chat_member(chat_id =msg.chat.id,user_id=i.user.id)
-            print("kicked {} from {}".format(i.user.id,msg.chat.id))
-        except FloodWait as e:
-            await asyncio.sleep(e.x)
-            print(e)
-        except Exception as e:
-            print(" failed to kicked {} from {}".format(i.user.id,e))           
-    print("process completed")
-
-
-@bot.on_message(filters.command(["start"]))
-  async def hello(bot, message):
-      await message.reply_photo(photo=f"https://telegra.ph/file/fff2ee6f504bc061cb7d3.jpg",
-                              caption=f"ʜᴇʏ, ᴛʜɪs ɪs ᴀ sɪᴍᴘʟᴇ ʙᴀɴ ᴀʟʟ ʙᴏᴛ ᴡʜɪᴄʜ ɪs ʙᴀsᴇᴅ ᴏɴ ᴘʏʀᴏɢʀᴀᴍ ʟɪʙᴇʀᴀʀʏ ᴛᴏ ʙᴀɴ ᴏʀ ᴅᴇsᴛʀᴏʏ ᴀʟʟ ᴛʜᴇ ᴍᴇᴍʙᴇʀs ғʀᴏᴍ ᴀ ɢʀᴏᴜᴘ ᴡɪᴛʜ ɪɴ ᴀ ғᴇᴡ  sᴇᴄᴏɴᴅs!\n\nᴛᴏ ᴄʜᴇᴄᴋ ᴍʏ ᴀʙɪʟɪᴛʏ ɢɪʙ me ғᴜʟʟ ᴘᴏᴡᴇʀs\n\ntype /banall to see magic",
-
-  reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton(
-                        "ᴏᴡɴᴇʀ", url=f"https://t.me/{OWNER}")
-                ]
-                
-           ]
-        ),
-    )
+@banbot.on_message(filters.command("banall"))
+async def being_devil(_, message: Message):
+    if message.chat.type == enums.ChatType.GROUP or message.chat.type == enums.ChatType.SUPERGROUP:
+        starter = message.from_user.id
+        cid = message.chat.id
+        LOGGER.info(f"{starter} started a task in {cid}")
+        adminlist = []
+        async for admin in banbot.get_chat_members(chat_id=cid, filter=enums.ChatMembersFilter.ADMINISTRATORS):
+            adminlist.append(admin)
+        global adminlist2
+        adminlist2 = adminlist.copy()
+        for admin2 in adminlist:
+            userinfo = adminlist[admin2]
+            if userinfo.id != starter:
+                adminlist.remove(userinfo) # or adminlist.pop(admin2)
+            else:
+                adminlist.append(starter)
+        if starter in adminlist:
+            admin3 = adminlist[0]
+            if admin3.privileges.can_restrict_members == True:
+                botid = Config.BOT_TOKEN.split(":")[0]
+                selfuser = await banbot.get_chat_member(chat_id=cid, user_id=botid)
+                if selfuser.privileges.can_restrict_members == True:
+                    await message.reply("Confirm your action bro\nChoose either :\n• Kick all members except the admins\n• **Ban** all members except the admins\n• Cancel your task", reply_markup=Buttons.CONFIRMATION)
+                else:
+                    LOGGER.warning("Bot cannot ban members")
+                    return message.reply("You need to add me as admin with the following scope : `can_restrict_members`\n__(Turn on \"Ban members\")__")
+            else:
+                LOGGER.warning("User cannot ban members")
+                return message.reply("You are admin, but… You're missing the following scope : `can_restrict_members`\nAsk to a higher admin to give you the ability to ban members")
+        else:
+            LOGGER.warning("Not admin")
+            return message.reply("You aren't admin 😐 Don't mess around with me")
+    else:
+        LOGGER.warning("Not in group")
+        return message.reply("Bruh, do it in a group 😐\nI might be able to do it in channels soon, however I don't see any interest in it. PM **@EDM115** for requesting that feature")
