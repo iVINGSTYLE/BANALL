556d66c41ccf134f54b23653d153860b7715b1e3
Bikash Halder <115603304+BikashHalderNew@users.noreply.github.com>
2024-02-07 17:54:25 +0530

    Update __main__.py

--- kisan/__main__.py
+++ kisan/__main__.py
@@ -1,17 +1,64 @@
-from pyrogram import idle
-from kisan import config, app
-from pyrogram import Client
+import os
+import logging
+from os import getenv
+from pyrogram import Client, filters, idle
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram.errors import ChatAdminRequired
 
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+)
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
+# config vars
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+OWNER = os.getenv("OWNER")
 
 # pyrogram client
 app = Client(
-          "banall",
-          api_id=API_ID,
-          api_hash=API_HASH,
-          bot_token=BOT_TOKEN,
+            "banall",
+            api_id=API_ID,
+            api_hash=API_HASH,
+            bot_token=BOT_TOKEN,
 )
 
+@app.on_message(
+filters.command("start")
+& filters.private            
+)
+async def start_command(client, message: Message):
+  await message.reply_photo(
+                            photo = f"https://telegra.ph/file/fff2ee6f504bc061cb7d3.jpg",
+                            caption = f"ʜᴇʏ, ᴛʜɪs ɪs ᴀ sɪᴍᴘʟᴇ ʙᴀɴ ᴀʟʟ ʙᴏᴛ ᴡʜɪᴄʜ ɪs ʙᴀsᴇᴅ ᴏɴ ᴘʏʀᴏɢʀᴀᴍ ʟɪʙᴇʀᴀʀʏ ᴛᴏ ʙᴀɴ ᴏʀ ᴅᴇsᴛʀᴏʏ ᴀʟʟ ᴛʜᴇ ᴍᴇᴍʙᴇʀs ғʀᴏᴍ ᴀ ɢʀᴏᴜᴘ ᴡɪᴛʜ ɪɴ ᴀ ғᴇᴡ  sᴇᴄᴏɴᴅs!\n\nᴛᴏ ᴄʜᴇᴄᴋ ᴍʏ ᴀʙɪʟɪᴛʏ ɢɪʙ me ғᴜʟʟ ᴘᴏᴡᴇʀs\n\nᴛʏᴘᴇ /ʙᴀɴᴀʟʟ ᴛᴏ ꜱᴇᴇ ᴍᴀɢɪᴄ ɪɴ ɢʀᴏᴜᴘ.",
+  reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "ᴏᴡɴᴇʀ", url=f"https://t.me/{OWNER}")
+                ]       
+           ]
+      )
+)
+
+@app.on_message(
+filters.command("banall") 
+& filters.group
+)
+async def banall_command(client, message: Message):
+    print("getting memebers from {}".format(message.chat.id))
+    async for i in app.get_chat_members(message.chat.id):
+        try:
+            await app.ban_chat_member(chat_id = message.chat.id, user_id = i.user.id)
+            print("kicked {} from {}".format(i.user.id, message.chat.id))
+        except Exception as e:
+            print("failed to kicked {} from {}".format(i.user.id, e))           
+    print("process completed")
+    
+
+# start bot client
 app.start()
-print("Bot Booted Successfully")
+print("Banall-Bot Booted Successfully")
 idle()
